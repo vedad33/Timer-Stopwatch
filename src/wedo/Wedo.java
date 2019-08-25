c0e520c788664ae912e6b072b0256bb8876d0bc4
vedad33 <50501516+vedad33@users.noreply.github.com>
2019-08-25 18:06:18 +0200

    commit4

---
+++ src/wedo/Wedo.java
@@ -0,0 +1,17 @@
+
+package wedo;
+
+
+public class Wedo {
+
+  
+    public static void main(String[] args) {
+        
+        Sa s = new Sa();
+        s.setVisible(true);
+        s.setSize(500,400);
+        
+    
+    }
+    
+}
